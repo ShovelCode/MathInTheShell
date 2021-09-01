25a6784f484754e43e10fa5485c15cfb18eaf567
Christopher Ray <vde435@gmail.com>
2021-08-31 21:00:50 -0500

    Update

--- ACos/ACos.c
+++ ACos/ACos.c
@@ -1,8 +1,10 @@
 #include <math.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main (void){
-	printf("testing");
+int main (int argc, char *argv[]){
+	printf("acos of X : %6.9f", acos(strtod(argv[1], NULL)));
 	
 	return 0;
 }
